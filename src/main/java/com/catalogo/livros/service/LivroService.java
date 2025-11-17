661366c21cb7f781041c62a1953ebd1865b67377
Clara Andrade <mariaclaraandrade140@gmail.com>
2025-11-17 10:42:44 -0300

    subindo projeto

---
+++ src/main/java/com/catalogo/livros/service/LivroService.java
@@ -0,0 +1,41 @@
+package com.catalogo.livros.service;
+
+import com.catalogo.livros.model.Livro;
+import com.catalogo.livros.repository.LivroRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LivroService {
+
+    private final LivroRepository repository;
+
+    public LivroService(LivroRepository repository) {
+        this.repository = repository;
+    }
+
+    public Livro salvar(Livro livro) {
+        return repository.save(livro);
+    }
+
+    public List<Livro> listar() {
+        return repository.findAll();
+    }
+
+    public Livro buscarPorId(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+
+    public List<Livro> buscarPorTitulo(String titulo) {
+        return repository.findByTituloContaining(titulo);
+    }
+
+    public List<Livro> buscarPorCategoria(String categoria) {
+        return repository.findByCategoria(categoria);
+    }
+}
